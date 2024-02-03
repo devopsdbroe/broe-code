b6b585a2c6c933a58344dab37e523b72ca6ba77d
devopsdbroe <dbroe1995@gmail.com>
2024-02-03 09:50:08 -0500

    Add dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
 	const path = useLocation().pathname;
+	const dispatch = useDispatch();
 
 	const { currentUser } = useSelector((state) => state.user);
+	const { theme } = useSelector((state) => state.theme);
 
 	return (
 		<Navbar className="border-b-2">
@@ -42,8 +45,9 @@ export default function Header() {
 					className="w-12 h-10 hidden sm:inline"
 					color="gray"
 					pill
+					onClick={() => dispatch(toggleTheme())}
 				>
-					<FaMoon />
+					{theme === "light" ? <FaSun /> : <FaMoon />}
 				</Button>
 				{currentUser ? (
 					<Dropdown
---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+
+export default function ThemeProvider({ children }) {
+	const { theme } = useSelector((state) => state.theme);
+
+	return (
+		<div className={theme}>
+			<div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[#10172A] min-h-screen">
+				{children}
+			</div>
+		</div>
+	);
+}
