720803e5fec4f71dfece1dc5d8807243045d9fb3
devopsdbroe <dbroe1995@gmail.com>
2024-02-02 19:44:16 -0500

    Create footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
 	return (
@@ -37,6 +38,7 @@ export default function App() {
 					element={<Projects />}
 				/>
 			</Routes>
+			<Footer />
 		</BrowserRouter>
 	);
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
 				{/* TODO: Create separate Logo component? */}
 				<span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
 					Football
-				</span>{" "}
+				</span>
 				Blog
 			</Link>
 			<form>
