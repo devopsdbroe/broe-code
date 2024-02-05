d63db7c763664807a78bc1f69cee0cec5fda9ef9
devopsdbroe <dbroe1995@gmail.com>
2024-02-05 18:52:30 -0500

    Create API to get posts

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -28,3 +28,50 @@ export const create = async (req, res, next) => {
 		next(error);
 	}
 };
+
+export const getPosts = async (req, res, next) => {
+	try {
+		const startIndex = parseInt(req.query.startIndex) || 0;
+		const limit = parseInt(req.query.limit) || 9;
+		const sortDirection = req.query.order === "asc" ? 1 : -1;
+		// Search posts based on query parameters
+		const posts = await Post.find({
+			...(req.query.userId && { userId: req.query.userId }),
+			...(req.query.category && { category: req.query.category }),
+			...(req.query.slug && { slug: req.query.slug }),
+			...(req.query.postId && { _id: req.query.postId }),
+			...(req.query.searchTerm && {
+				$or: [
+					{ title: { $regex: req.query.searchTerm, $options: "i" } },
+					{ content: { $regex: req.query.searchTerm, $options: "i" } },
+				],
+			}),
+		})
+			.sort({ updatedAt: sortDirection })
+			.skip(startIndex)
+			.limit(limit);
+
+		// Get total posts in DB
+		const totalPosts = await Post.countDocuments();
+
+		// Calculate one month ago to get posts from last month
+		const now = new Date();
+		const oneMonthAgo = new Date(
+			now.getFullYear(),
+			now.getMonth() - 1,
+			now.getDate()
+		);
+		const lastMonthPosts = await Post.countDocuments({
+			createdAt: { $gte: oneMonthAgo },
+		});
+
+		// Return all posts, total posts, and total posts in the last month
+		res.status(200).json({
+			posts,
+			totalPosts,
+			lastMonthPosts,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
