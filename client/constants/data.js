c044adc8aa11025674d66d29b95ee508aa48a012
devopsdbroe <dbroe1995@gmail.com>
2024-03-21 20:03:16 -0400

    Adjust categories + add python

--- client/constants/data.js
+++ client/constants/data.js
@@ -1,8 +1,9 @@
 export const topics = [
-	{ id: 1, value: "html", name: "HTML" },
-	{ id: 2, value: "css", name: "CSS" },
+	{ id: 1, value: "html/css", name: "HTML/CSS" },
+	{ id: 2, value: "python", name: "Python" },
 	{ id: 3, value: "javascript", name: "JavaScript" },
 	{ id: 4, value: "tailwind", name: "Tailwind" },
 	{ id: 5, value: "react", name: "React" },
 	{ id: 6, value: "nextjs", name: "Next.js" },
+	{ id: 6, value: "other", name: "Other" },
 ];
