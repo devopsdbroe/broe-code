e10709d287371634cd39016c814bfad202091505
devopsdbroe <dbroe1995@gmail.com>
2024-04-05 17:26:07 -0400

    Update "About me" section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,23 +8,24 @@ export default function About() {
 					</h1>
 					<div className="text-md text-gray-500 flex flex-col gap-6">
 						<p>
-							Welcome to Broe Code where I document my attempt at becoming a
-							full stack web developer! Here, I&apos;ll share the projects
-							I&apos;ve been working on, and the lessons I learn along the way.
+							Welcome to Broe Code, where I document my attempt at becoming a
+							full-stack web developer! Here, I&apos;ll share the projects
+							I&apos;ve been working on and the lessons I&apos;ve learned along
+							the way.
 						</p>
 						<p>
-							I plan to post here as frequently as possible. It&apos;s open to
-							interpretation if that means only once a week or several times a
-							day. It could be a bit of both depending how I&apos;m feeling on a
-							given day.
+							I plan to post here frequently. It&apos;s open to interpretation
+							whether that means only once a week or several times a day.
+							Depending on how I&apos;m feeling on a given day, it could be a
+							bit of both.
 						</p>
 						<p>
 							I&apos;ve found that discussion is one of the most effective ways
 							to learn, so if I&apos;ve posted an article with incorrect
 							information, I encourage you to let me know (politely, of course
-							🙂). I also welcome you to share your opinions on all of the
-							topics I cover here. This would be a very boring place if we
-							agreed on everything!
+							🙂). I also welcome you to share your opinions on the topics I
+							cover here. This blog would be a very dull place if we agreed on
+							everything!
 						</p>
 					</div>
 				</div>
