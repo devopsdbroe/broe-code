918e3669f1d5df959c82b6eb74ec3fee2c8a6be3
Daniel Broe <dbroe1995@gmail.com>
2024-06-03 18:40:21 -0400

    Update packages + blog topics

--- client/constants/data.js
+++ client/constants/data.js
@@ -5,5 +5,5 @@ export const topics = [
 	{ id: 4, value: "tailwind", name: "Tailwind" },
 	{ id: 5, value: "react", name: "React" },
 	{ id: 6, value: "nextjs", name: "Next.js" },
-	{ id: 6, value: "other", name: "Other" },
+	{ id: 7, value: "other", name: "Other" },
 ];
