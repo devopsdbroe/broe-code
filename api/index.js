c0fadd80850517151a29802f15040dddd25f358c
Daniel Broe <dbroe1995@gmail.com>
2024-06-03 20:56:42 -0400

    Add "path" import

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
